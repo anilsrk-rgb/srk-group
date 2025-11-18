4f338fe6095a44f30922a84751f8da6de049a18b
anilsrk-rgb <g26091993@gmail.com>
2025-11-18 16:47:34 +0530

    Delete scripts.js

--- scripts.js
+++
@@ -1,8 +0,0 @@
-
-// Smooth scroll for anchor links
-document.querySelectorAll('a[href^="#"]').forEach(a=>{
-  a.addEventListener('click',function(e){
-    e.preventDefault();
-    document.querySelector(this.getAttribute('href')).scrollIntoView({behavior:'smooth'});
-  });
-});
